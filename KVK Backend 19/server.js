4063a32b96e602943528bdfaa39fcf5557d6e07b
Hemant Mali <hemantmali8233@gmail.com>
2026-03-06 08:15:09 +0530

    Update_project_with_deployment_ready_configs_and_clean_styles

--- KVK Backend 19/server.js
+++ KVK Backend 19/server.js
@@ -32,6 +32,11 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
